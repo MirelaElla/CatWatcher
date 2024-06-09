79c42c7e50345caa9e160120f26fa1332e4bdfb1
MirelaElla <dubravac.mirela@hotmail.com>
2024-06-09 23:59:13 +0200

    improved models, model comparison, test images, imagesizing comparison

--- f_converter.py
+++ f_converter.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
 # Load the model
-model = tf.keras.models.load_model('models/best_model_hrs3.h5')
+model = tf.keras.models.load_model('models/model_hrs6.h5')
 
 # Convert the model
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 tflite_model = converter.convert()
 
 # Save the model to disk
-with open('best_model_hrs3.tflite', 'wb') as f_out:
+with open('models/model_hrs6.tflite', 'wb') as f_out:
     f_out.write(tflite_model)
